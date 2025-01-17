648e9d89125e08334c67fdabd52d3158fccf51b5
Deodato Moreira <deodatomoreira07@gmail.com>
2025-01-17 11:01:59 -0300

    feat: Iniciando projeto de gerenciamento de tarefas

---
+++ src/main/java/com/gerenciamento/gerenciadortarefas/controller/RotasController.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.gerenciadortarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RotasController {
+    @GetMapping("/adm")
+    public String administracao(Model model) {
+        return "gerenciadortarefas/gerenciador-tarefas";
+    }
+}
